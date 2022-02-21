3c12b58201e68ca62287cf92643e041fcd2decbf
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-02-21 11:19:52 +0100

    Add public subnet

--- terraform/webserver-vpc.tf
+++ terraform/webserver-vpc.tf
@@ -1,3 +1,9 @@
 resource "aws_vpc" "web-server-vpc" {
     cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id            = aws_vpc.web-server-vpc.id
+  availability_zone = "us-west-2a"
+  cidr_block        = "10.0.1.0/24"
+}
